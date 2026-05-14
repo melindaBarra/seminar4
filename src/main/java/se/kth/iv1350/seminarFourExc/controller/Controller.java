bef4b87e931fce64262412a8e90398aad11f27df
melin <melin@Datir.lan>
2026-05-14 17:44:22 +0200

    Adjusted ControllerTest error‑message tests.

--- src/main/java/se/kth/iv1350/seminarFourExc/controller/Controller.java
+++ src/main/java/se/kth/iv1350/seminarFourExc/controller/Controller.java
@@ -99,7 +99,7 @@ public class Controller {
             return repairOrder.getOrderId();
         } catch (DatabaseFailureException e) {
             logger.log(e.getMessage());
-            throw new OperationFailedException("Something went wrong! Could not register repair order", e);
+            throw new OperationFailedException("Something went wrong. Please try again later.", e);
         }
         
     }
